8422fdb10a71dbd64f0fcc5627f172d24bb8ce84
Subrata Biswas <subratabiswasofficial@gmail.com>
2021-06-21 17:37:44 +0530

    testing CICD

--- src/controllers/sample.ts
+++ src/controllers/sample.ts
@@ -1,7 +1,7 @@
 import { Response, Request } from 'express';
 
 const getApi = async (req: Request, res: Response) => {
-    return res.status(200).json({ message: 'Wroking' });
+    return res.status(200).json({ message: 'changes Wroking' });
 };
 
 export default {
